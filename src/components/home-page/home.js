613311368f00095d9f16a92d87ac25004781ffb0
avickers <avickers.iom@gmail.com>
2016-05-10 16:28:16 -0500

    SigDig fix

--- src/components/home-page/home.js
+++ src/components/home-page/home.js
@@ -8,15 +8,16 @@ define(["knockout", "text!./home.html", "jquery","moment"], function(ko, homeTem
 
 		self.threshold = ko.observable('50');
 		self.raised = ko.observable('$0');
+		self.amount = ko.observable(0);
 		self.prevRaised = ko.pureComputed(function() {
-			return '$' + self.pledgesTotal() * self.prevCallers() * 0.01;
+			return '$' +(self.amount() * self.prevCallers() * 0.01).toFixed(2);
 		});
-		self.pledgesTotal = ko.observable(50);
+		self.pledgesTotal = ko.observable(0);
 		self.perCaller = ko.pureComputed(function() {
 			return '$' + (self.pledgesTotal() * 0.01).toFixed(2);
 		});
-		self.prevCallers = ko.observable(100);
-		self.sumCallers = ko.observable(9001);
+		self.prevCallers = ko.observable(0);
+		self.sumCallers = ko.observable(0);
 		self.histCallers = ko.observableArray([]);
 
 		LoadData(self);
@@ -24,7 +25,7 @@ define(["knockout", "text!./home.html", "jquery","moment"], function(ko, homeTem
 
 	function LoadData(self) {
 		$.getJSON(baseURL+'pledges/?total', function(data) {
-			//console.debug(data);
+			self.amount(data.amount);
 			self.pledgesTotal(data.total);
 		});
 		$.getJSON(baseURL+'dailyCallLogs/total', function(data) {
